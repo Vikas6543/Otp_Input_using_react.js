5af2b7189b31ccd468ac45dbd7c5c190094a2cc9
vikase9bit <you@example.com>
2024-03-16 11:18:36 +0530

    phone number validation done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PhoneNumber from './components/PhoneNumber';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <PhoneNumber />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/PhoneNumber.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import OtpInput from './OtpInput';
+
+const PhoneNumber = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [showOtpInput, setShowOtpInput] = useState(false);
+
+  const handlePhoneNumber = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handlePhoneSubmit = (event) => {
+    event.preventDefault();
+
+    // phone validations
+    const regex = /[^0-9]/g;
+    if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+      alert('Invalid Phone Number');
+      return;
+    }
+
+    // Call BE API
+    // show OTP Field
+    setShowOtpInput(true);
+  };
+
+  const onOtpSubmit = (otp) => {
+    console.log('Login Successful', otp);
+  };
+
+  return (
+    <div>
+      {!showOtpInput && <p className='heading'>Enter Your Mobile Number</p>}
+
+      {!showOtpInput ? (
+        <form onSubmit={handlePhoneSubmit}>
+          <input
+            type='text'
+            value={phoneNumber}
+            onChange={handlePhoneNumber}
+            placeholder='Enter Phone Number'
+            className='phone-number-input'
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p className='enter-otp'>Enter OTP sent to {phoneNumber}</p>
+          <OtpInput length={4} onOtpSubmit={onOtpSubmit} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PhoneNumber;
